6d64c09cfe99d94447337040c1671af56a86ae54
kazu <129391973+kz7c@users.noreply.github.com>
2026-03-21 12:17:59 +0900

    update setting

--- TodoHub/Views/SettingPage.xaml.cs
+++ TodoHub/Views/SettingPage.xaml.cs
@@ -1,105 +1,120 @@
-namespace TodoHub.Views;
 using Microsoft.Maui.Storage;
-
-public partial class SettingPage : ContentPage
+using System.Text.RegularExpressions;
+namespace TodoHub.Views
 {
-	string token = string.Empty;
-    string repo = string.Empty;
-
-
-    public SettingPage()
-	{
-		InitializeComponent();
-	}
-
-
-    private void ChangeTokenBtn_Clicked(object sender, EventArgs e)// トークン変更ボタンの動作
+    public partial class SettingPage : ContentPage
     {
-        ChangeTokenBtn.IsVisible = false; // ボタン消す
-        ChangeTokenForm.IsVisible = true; // フォーム出す
-    }
+        // 入力受け取り用の変数
+        string token = string.Empty;
+        string repo = string.Empty;
 
 
-    [Obsolete]
-	private async void RegisterTokenBtn_Clicked(object? sender, EventArgs e)// トークンの登録ボタンの動作
-	{
-        // 登録ボタンの動作を無効化
-        RegisterTokenBtn.IsEnabled = false;
-
-        try
+        public SettingPage()
         {
-            token = ChangeTokenEntry.Text;
+            InitializeComponent();
+        }
 
-            if (string.IsNullOrEmpty(token))// トークンが空の場合はエラーメッセージを表示
+
+        private void ChangeTokenBtn_Clicked(object? sender, EventArgs e)// トークン変更ボタンの動作
+        {
+            ChangeTokenBtn.IsVisible = false; // ボタン消す
+            ChangeTokenForm.IsVisible = true; // フォーム出す
+        }
+
+
+        [Obsolete]
+        private async void RegisterTokenBtn_Clicked(object? sender, EventArgs e)// トークンの登録ボタンの動作
+        {
+            // 登録ボタンの動作を無効化
+            RegisterTokenBtn.IsEnabled = false;
+
+            try
             {
-                await DisplayAlertAsync("エラー", "トークンを入力してください。", "OK");
-                return;
+                token = ChangeTokenEntry.Text;
+
+                if (string.IsNullOrEmpty(token))// トークンが空の場合はエラーメッセージを表示
+                {
+                    await DisplayAlertAsync("エラー", "トークンを入力してください。", "OK");
+                    return;
+                }
+
+
+                // トークンをセキュアに保存
+                await SecureStorage.Default.SetAsync("github_token", token);
+                await DisplayAlertAsync("", "トークンの登録が完了しました。", "OK");
+
+                // 変更フォームを消して、トークン変更ボタンを表示
+                ChangeTokenForm.IsVisible = false;
+                ChangeTokenBtn.IsVisible = true;
             }
-
-
-            // トークンをセキュアに保存
-            await SecureStorage.Default.SetAsync("github_token", token);
-            await DisplayAlertAsync("", "トークンの登録が完了しました。", "OK");
-
-            // 変更フォームを消して、トークン変更ボタンを表示
-            ChangeTokenForm.IsVisible = false;
-            ChangeTokenBtn.IsVisible = true;
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlertAsync("エラー", "保存に失敗ました。", "OK");
-        }
-        finally
-        {
-            // 登録ボタンの動作を再有効化
-            RegisterTokenBtn.IsEnabled = true;
-        }
-    }
-
-
-
-    // -----------------------------------------------------------------------------------------------------
-    // -----------------------------------------------------------------------------------------------------
-
-    private void ChangeRepoBtn_Clicked(object sender, EventArgs e)// トークン変更ボタンの動作
-    {
-        ChangeRepoBtn.IsVisible = false; // ボタン消す
-        ChangeRepoForm.IsVisible = true; // フォーム出す
-    }
-
-
-    private async void RegisterRepoBtn_Clicked(object? sender, EventArgs e)// リポジトリの登録ボタンの動作
-    {
-        // 登録ボタンの動作を無効化
-        RegisterRepoBtn.IsEnabled = false;
-
-        try
-        {
-            repo = ChangeRepoEntry.Text;
-
-            if (string.IsNullOrEmpty(repo))// トークンが空の場合はエラーメッセージを表示
+            catch (Exception)
             {
-                await DisplayAlertAsync("エラー", "リポジトリ名を入力してください。", "OK");
-                return;
+                await DisplayAlertAsync("エラー", "保存に失敗ました。", "OK");
             }
-
-
-            // リポジトリ名を保存
-            Preferences.Default.Set("github_repo", repo);
-            await DisplayAlertAsync("", "リポジトリの登録が完了しました。", "OK");
-
-            // 変更フォームを消して、トークン変更ボタンを表示
-            ChangeRepoForm.IsVisible = false;
-            ChangeRepoBtn.IsVisible = true;
+            finally
+            {
+                // 登録ボタンの動作を再有効化
+                RegisterTokenBtn.IsEnabled = true;
+            }
         }
-        catch (Exception ex)
+
+
+
+        // -----------------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------
+
+
+        private void ChangeRepoBtn_Clicked(object? sender, EventArgs e)// リポジトリ変更ボタンの動作
         {
-            await DisplayAlertAsync("エラー", "保存に失敗ました。", "OK");
+            ChangeRepoBtn.IsVisible = false; // ボタン消す
+            ChangeRepoForm.IsVisible = true; // フォーム出す
+            if (!string.IsNullOrEmpty(Preferences.Default.Get("github_repo", "")))
+            {
+                ChangeRepoEntry.Text = Preferences.Default.Get("github_repo", "");
+            }
         }
-        finally
+
+
+        private async void RegisterRepoBtn_Clicked(object? sender, EventArgs e)// リポジトリの登録ボタンの動作
         {
-            // 登録ボタンの動作を再有効化
-            RegisterRepoBtn.IsEnabled = true;
+            // 登録ボタンの動作を無効化
+            RegisterRepoBtn.IsEnabled = false;
+
+            try
+            {
+                repo = ChangeRepoEntry.Text;
+                var RepoPattern = @"^[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?/[a-zA-Z0-9._-]+$";
+
+                if (!Regex.IsMatch(repo, RepoPattern))
+                {
+                    await DisplayAlertAsync("エラー", "正しいリポジトリ名を入力してください。", "OK");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(repo))// トークンが空の場合はエラーメッセージを表示
+                {
+                    await DisplayAlertAsync("エラー", "リポジトリ名を入力してください。", "OK");
+                    return;
+                }
+
+
+                // リポジトリ名を保存
+                Preferences.Default.Set("github_repo", repo);
+                await DisplayAlertAsync("", "リポジトリの登録が完了しました。", "OK");
+
+                // 変更フォームを消して、トークン変更ボタンを表示
+                ChangeRepoForm.IsVisible = false;
+                ChangeRepoBtn.IsVisible = true;
+            }
+            catch (Exception)
+            {
+                await DisplayAlertAsync("エラー", "保存に失敗ました。", "OK");
+            }
+            finally
+            {
+                // 登録ボタンの動作を再有効化
+                RegisterRepoBtn.IsEnabled = true;
+            }
         }
     }
 }
